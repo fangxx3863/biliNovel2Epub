d32d296cd4656d4d3b954dbb0b891fa2f43ec13e
fangxx3863 <2221048564@qq.com>
2022-05-01 17:14:18 +0800

    Fix Bugs

--- bilinovel2epub.py
+++ bilinovel2epub.py
@@ -6,6 +6,9 @@ import os
 import sys
 import shutil
 from rich.console import Console
+from rich.prompt import IntPrompt
+from rich.prompt import Prompt
+from rich.prompt import Confirm
 from ebooklib import epub
 import uuid
 import download
@@ -13,7 +16,9 @@ from download import download
 import ssl
 from PIL import Image
 import io
-from multiprocessing import Process, Pool
+from random import randint
+from multiprocessing import Pool, set_start_method
+set_start_method('spawn', force=True)
 
 console = Console()
 session = requests.Session()
@@ -24,10 +29,68 @@ book = epub.EpubBook()
 
 基础URL = "https://w.linovelib.com"
 
+ 
+USER_AGENTS = [
+                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+                "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+                "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+                "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+                "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+                "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+                "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+                "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+                "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+                "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+                "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+                "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+                "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+                "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; InfoPath.3; rv:11.0) like Gecko",
+                "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+                "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+                "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+                "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+                "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+                "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+                "Mozilla/5.0 (iPod; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+                "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5",
+                "Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                "MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+                "Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10",
+                "Mozilla/5.0 (Linux; U; Android 3.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13",
+                "Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+",
+                "Mozilla/5.0 (hp-tablet; Linux; hpwOS/3.0.0; U; en-US) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/233.70 Safari/534.6 TouchPad/1.0",
+                "Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 NokiaN97-1/20.0.019; Profile/MIDP-2.1 Configuration/CLDC-1.1) AppleWebKit/525 (KHTML, like Gecko) BrowserNG/7.1.18124",
+                "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0; HTC; Titan)",
+                "UCWEB7.0.2.37/28/999",
+                "NOKIA5700/ UCWEB7.0.2.37/28/999",
+                "Openwave/ UCWEB7.0.2.37/28/999",
+                "Mozilla/4.0 (compatible; MSIE 6.0; ) Opera/UCWEB7.0.2.37/28/999",
+                "Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25",
+             ]
+ 
+random_agent = USER_AGENTS[randint(0, len(USER_AGENTS)-1)]
 HEARDERS = {
     "cookie": "_ga=GA1.2.373713668.1646927652; _gid=GA1.2.1447053390.1651231171; Hm_lpvt_d29ecd95ff28d58324c09b9dc0bee919=1651231349; Hm_lvt_d29ecd95ff28d58324c09b9dc0bee919=1649823562,1651231165; jieqiUserInfo=jieqiUserId%3D627182%2CjieqiUserUname%3Dfangxx3863%2CjieqiUserName%3Dfangxx3863%2CjieqiUserGroup%3D3%2CjieqiUserGroupName%3D%E6%99%AE%E9%80%9A%E4%BC%9A%E5%91%98%2CjieqiUserVip%3D0%2CjieqiUserHonorId%3D1%2CjieqiUserHonor%3D%E5%A4%A9%E7%84%B6%2CjieqiUserToken%3D8ea5ef793d94938673124b15cb3a7102%2CjieqiCodeLogin%3D0%2CjieqiCodePost%3D0%2CjieqiUserPassword%3D5c82b131f01843ca05e751717d74a992%2CjieqiUserLogin%3D1651231169; jieqiVisitId=article_articleviews%3D2939; jieqiVisitInfo=jieqiUserLogin%3D1651231169%2CjieqiUserId%3D627182; night=0; PHPSESSID=bsdrsrdj916v5etol006ji2odl",
     "referer": "https://w.linovelib.com/",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+    "user-agent": random_agent,
 }
 
 def 标准化JSON(s:str)->dict:
@@ -38,14 +101,14 @@ def 标准化JSON(s:str)->dict:
 def 下载文件(url, path='file'):
     if isinstance(url, str):
         try:
-            download(url, os.getcwd() + '/' + str(path) + '/' + os.path.basename(url), replace=False, verbose=False)
+            download(url, os.getcwd() + '/' + str(path) + '/' + os.path.basename(url), replace=False, verbose=False, timeout=5)
         except:
             return url
     if isinstance(url, list):
         errUrls = []
         for i in url:
             try:
-                download(i, os.getcwd() + '/' + str(path) + '/' + os.path.basename(i), replace=False, verbose=False)
+                download(i, os.getcwd() + '/' + str(path) + '/' + os.path.basename(i), replace=False, verbose=False, timeout=5)
             except:
                 errUrls.append(i)
                 return errUrls
@@ -68,11 +131,13 @@ def 写到书本(title, author, content, cover_name, cover_file, imgDir, folder=
     book.spine = ["nav", ]
     IDS = -1
     for 卷名 in content:
+        console.print("卷: " + 卷名)
         卷名标题 = "<h1>" + 卷名 + "</h1>"
         写入内容 = 写入内容 + 卷名标题
         book.toc.append([epub.Section(卷名), []])
         IDS += 1
         for 章节 in content[卷名]:
+            console.print("章节: " + 章节[0])
             单页 = epub.EpubHtml(title = 章节[0],
                        file_name = 章节[0] + ".xhtml",
                        lang = "zh")
@@ -151,8 +216,17 @@ if __name__ == "__main__":
             子章节.append([文本,url])
     目录[缓存] = 子章节
     
+    内容 = dict()
+    图片URL集合 = []
+    IDS = -1
     for 卷名 in 目录:
+        console.print("卷: " + 卷名)
+        IDS = -1
         for 章节 in 目录[卷名]:
+            内容.setdefault(卷名, []).append([章节[0]])
+            IDS += 1
+            console.print("章节: " + 章节[0])
+            缓存内容 = ""
             章节标题 = 章节[0]
             章节URL = 章节[1]
 
@@ -173,17 +247,7 @@ if __name__ == "__main__":
                     章节.append(下一个URL)
                 else:
                     break
-                
-    内容 = dict()
-    图片URL集合 = []
-    IDS = -1
-    for 卷名 in 目录:
-        console.print("卷: " + 卷名)
-        for 章节 in 目录[卷名]:
-            内容.setdefault(卷名, []).append([章节[0]])
-            IDS += 1
-            console.print("章节: " + 章节[0])
-            缓存内容 = ""
+            
             for 单章URL in 章节[1:]:
                 soup = BeautifulSoup(session.get(单章URL,headers=HEARDERS).text, "lxml")
                 图片集合 = soup.find_all("img")
@@ -195,8 +259,11 @@ if __name__ == "__main__":
                 缓存内容 = 缓存内容 + 文章内容
                 console.print(f"正在处理: {单章URL}")
             内容[卷名][IDS].append(缓存内容)
-
-    下载图片集合(图片URL集合, 16)
+            
+    if Confirm.ask("是否下载图片? 下载图片[Y]不下载[N] "):
+        console.print(图片URL集合)
+        下载图片集合(图片URL集合, 8)
+        
     下载文件(封面URL)
     写到书本(书名, 作者, 内容, "cover", "file/"+封面URL.split("/")[-1], "file")
     shutil.rmtree('file')
