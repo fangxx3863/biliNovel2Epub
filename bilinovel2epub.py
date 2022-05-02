2abdead471203d25379b9b13614cc16f50844290
fangxx3863 <2221048564@qq.com>
2022-05-02 17:29:07 +0800

    Fix Bugs

--- bilinovel2epub.py
+++ bilinovel2epub.py
@@ -102,7 +102,7 @@ def 标准化JSON(s:str)->dict:
 # 下载函数
 def 下载文件(url, path='file'):
     if isinstance(url, str):
-        urlFile = Path(url.split("/")[-1])
+        urlFile = Path(path + "/" + url.split("/")[-1])
         if urlFile.exists():
             pass
         else:
@@ -116,7 +116,7 @@ def 下载文件(url, path='file'):
     if isinstance(url, list):
         errUrls = []
         for i in url:
-            urlFile = Path(i.split("/")[-1])
+            urlFile = Path(path + "/" + i.split("/")[-1])
             if urlFile.exists():
                 pass
             else:
@@ -136,7 +136,7 @@ def 下载图片集合(urls, jobs):
     while errUrls:
         for i in errUrls:
             try:
-                os.remove(str(i).split("src=\"")[-1][:-3].split("/")[-1] + ".part")
+                os.remove("file/" + str(i).split("src=\"")[-1][:-3].split("/")[-1] + ".part")
             except:
                 continue
         errUrls = 下载文件(errUrls)
