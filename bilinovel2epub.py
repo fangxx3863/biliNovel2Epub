d7b783150a3c3ea0aaf5e1fbcafc7256790a2f17
可汗 <njyukehan@gmail.com>
2023-07-20 20:59:11 +0800

    Update bilinovel2epub.py

--- bilinovel2epub.py
+++ bilinovel2epub.py
@@ -20,7 +20,6 @@ import re
 from random import randint
 from multiprocessing import Pool, set_start_method
 import multiprocessing
-from getContent import getContent
 #set_start_method('spawn', force=True)
 
 console = Console()
@@ -61,6 +60,132 @@ HEARDERS = {
     "referer": "https://w.linovelib.com/",
     "user-agent": random_agent,
 }
+
+# 原始的中文字符混淆对应map
+secretMap = {
+    "\uE800": "的",
+    "\uE801": "一",
+    "\uE802": "是",
+    "\uE803": "了",
+    "\uE804": "我",
+    "\uE805": "不",
+    "\uE806": "人",
+    "\uE807": "在",
+    "\uE808": "他",
+    "\uE809": "有",
+    "\uE80A": "这",
+    "\uE80B": "个",
+    "\uE80C": "上",
+    "\uE80D": "们",
+    "\uE80E": "来",
+    "\uE80F": "到",
+    "\uE810": "时",
+    "\uE811": "大",
+    "\uE812": "地",
+    "\uE813": "为",
+    "\uE814": "子",
+    "\uE815": "中",
+    "\uE816": "你",
+    "\uE817": "说",
+    "\uE818": "生",
+    "\uE819": "国",
+    "\uE81A": "年",
+    "\uE81B": "着",
+    "\uE81C": "就",
+    "\uE81D": "那",
+    "\uE81E": "和",
+    "\uE81F": "要",
+    "\uE820": "她",
+    "\uE821": "出",
+    "\uE822": "也",
+    "\uE823": "得",
+    "\uE824": "里",
+    "\uE825": "后",
+    "\uE826": "自",
+    "\uE827": "以",
+    "\uE828": "会",
+    "\uE829": "家",
+    "\uE82A": "可",
+    "\uE82B": "下",
+    "\uE82C": "而",
+    "\uE82D": "过",
+    "\uE82E": "天",
+    "\uE82F": "去",
+    "\uE830": "能",
+    "\uE831": "对",
+    "\uE832": "小",
+    "\uE833": "多",
+    "\uE834": "然",
+    "\uE835": "于",
+    "\uE836": "心",
+    "\uE837": "学",
+    "\uE838": "么",
+    "\uE839": "之",
+    "\uE83A": "都",
+    "\uE83B": "好",
+    "\uE83C": "看",
+    "\uE83D": "起",
+    "\uE83E": "发",
+    "\uE83F": "当",
+    "\uE840": "没",
+    "\uE841": "成",
+    "\uE842": "只",
+    "\uE843": "如",
+    "\uE844": "事",
+    "\uE845": "把",
+    "\uE846": "还",
+    "\uE847": "用",
+    "\uE848": "第",
+    "\uE849": "样",
+    "\uE84A": "道",
+    "\uE84B": "想",
+    "\uE84C": "作",
+    "\uE84D": "种",
+    "\uE84E": "开",
+    "\uE84F": "美",
+    "\uE850": "乳",
+    "\uE851": "阴",
+    "\uE852": "液",
+    "\uE853": "茎",
+    "\uE854": "欲",
+    "\uE855": "呻",
+    "\uE856": "肉",
+    "\uE857": "交",
+    "\uE858": "性",
+    "\uE859": "胸",
+    "\uE85A": "私",
+    "\uE85B": "穴",
+    "\uE85C": "淫",
+    "\uE85D": "臀",
+    "\uE85E": "舔",
+    "\uE85F": "射",
+    "\uE860": "脱",
+    "\uE861": "裸",
+    "\uE862": "骚",
+    "\uE863": "唇",
+}
+
+# 恢复函数，根据secretMap进行恢复
+def restore_chars(text):
+        restored_text = ""
+        i = 0
+        while i < len(text):
+                char = text[i]
+                if char in secretMap:
+                        restored_text += secretMap[char]
+                else:
+                        restored_text += char
+                i += 1
+        return restored_text
+
+def getContent(URL):
+        soup = BeautifulSoup(session.get(URL, headers=HEARDERS, timeout=5).text, "lxml")
+        content = soup.find(id='acontent')
+        div_cgo = soup.find('div', {'class': 'cgo'})
+        if div_cgo:
+                div_cgo.extract()
+        return restore_chars(str(content))
+
 
 def 标准化JSON(s:str)->dict:
     obj = eval(s, type('js', (dict,), dict(__getitem__=lambda s, n: n))())
@@ -416,7 +541,7 @@ def 主要():
                 # 如果不存在则创建目录
                 os.makedirs("file")
             下载图片集合(图片URL集合[卷名], 4)
-            写到书本(书名+"_"+卷名, 作者, 内容[卷名], "-".join(clean_file_name(封面URL).split("/")[-4:]), "file/" + "-".join(clean_file_name(封面URL).split("/")[-4:]), "file", 书名, True)
+            写到书本(书名+" "+卷名, 作者, 内容[卷名], "-".join(clean_file_name(封面URL).split("/")[-4:]), "file/" + "-".join(clean_file_name(封面URL).split("/")[-4:]), "file", 书名, True)
             try:
                 shutil.rmtree('file')
             except:
